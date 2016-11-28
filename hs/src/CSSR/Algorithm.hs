88c042cbc3bc91a8ba9551c8df28c2c19fc7a1f8
stites <sam@stites.io>
2016-11-28 18:52:13 -0500

    add comments for next part of algorithm

--- hs/src/CSSR/Algorithm.hs
+++ hs/src/CSSR/Algorithm.hs
@@ -25,4 +25,60 @@ main filepath = do
 initialization :: String -> HistTree
 initialization = uncurry convert . M.buildTree 4 . V.fromList
 
-
+-------------------------------------------------------------------------------
+-- | == Phase II: "Growing a Looping Tree" algorithm
+--
+-- INIT root looping node
+-- INIT queue of active, unchecked nodes
+-- QUEUE root
+-- WHILE queue is not empty
+--   DEQUEUE first looping node from the queue
+--   COMPUTE homogeneity(dequeued looping node, parse tree)
+--   IF node is homogeneous
+--   THEN continue
+--   ELSE
+--     CONSTRUCT new looping nodes for all valid children (one for each symbol in
+--               alphabet - must have empirical observation in dataset).
+--     FOR each new node constructed
+--       COMPUTE excisability(node, looping tree)
+--       COMPUTE isEdge(node, looping tree)
+--       ADD all new looping nodes to children of active node (mapped by symbol)
+--       ADD unexcisable children to queue (FIXME: what about edgesets?)
+--   ENDIF
+-- ENDWHILE
+--
+-- isEdge:
+--   INPUTS: looping node, looping tree
+--   COLLECT all terminal nodes that are not ancestors
+--   IF exists terminal nodes with identical distributions
+--   THEN
+--     mark looping node as an edge set
+--     mark found terminals as an edge set
+--     // We will merge edgesets in Phase III.
+--   ENDIF
+--
+-- Homogeneity:
+--   INPUTS: looping node, parse tree
+--   COLLECT all next-step histories from looping node in parse tree
+--   FOR each history in next-step histories
+--     FOR each child in history's children
+--       IF child's distribution ~/=  node's distribution
+--       THEN RETURN false
+--       ENDIF
+--     ENDFOR
+--   ENDFOR
+--   RETURN TRUE
+--
+-- Excisability:
+--   INPUTS: looping node, looping tree
+--   COLLECT all ancestors of the looping node from the looping tree, ordered by
+--           increasing depth (depth 0, or "root node," first)
+--   FOR each ancestor
+--     IF ancestor's distribution == looping node's distribution
+--     THEN
+--       the node is excisable: create loop in the tree
+--       ENDFOR (ie "break")
+--     ELSE do nothing
+--     ENDIF
+--   ENDFOR
+-------------------------------------------------------------------------------
